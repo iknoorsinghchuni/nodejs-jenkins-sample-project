e319e82a2d3e2229384928c4dbf6ff2de056cd65
iknoorsinghchuni <100800884+iknoorsinghchuni@users.noreply.github.com>
2022-10-11 03:01:17 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo " Changing working dir "
-cd /home/ubuntu
+cd 
 
 echo " Cloninng latest code "
 git clone https://github.com/ellitus/nodejs-jenkins-sample-project.git
@@ -10,5 +10,4 @@ echo " Installing packages "
 npm install
 
 echo " Restart node server "
-pm2 stop all
 pm2 start index.js
