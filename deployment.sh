27949f58d034a6cbc48771bf2eab356716cf969a
iknoorsinghchuni <100800884+iknoorsinghchuni@users.noreply.github.com>
2022-10-11 03:52:10 -0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -5,4 +5,4 @@ echo "Deploying on ubuntu VM"
 echo "**********************"
 echo " Connecting to ubuntu "
 chmod +x update.sh
-ssh -i /var/jenkins_home/password.pem ubuntu@ec2-18-206-115-80.compute-1.amazonaws.com 'bash -s' < update.sh
+ssh -i devops@192.168.1.144 'bash -s' < update.sh
