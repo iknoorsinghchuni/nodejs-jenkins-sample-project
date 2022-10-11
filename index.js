8953c381918e40113d4e84e07d8f606f4a6785c1
iknoorsinghchuni <100800884+iknoorsinghchuni@users.noreply.github.com>
2022-10-11 02:09:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var express = require('express')
-var app = express()
+var http = require("http");
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello Jenkins Node is successfully working\n');
+}).listen(8081);
 
-app.listen(8001, function () {
-    console.log('App listening on port 8001!')
-})
+// Console will print the message
+console.log('Server running at http://192.168.1.144:8081/');
